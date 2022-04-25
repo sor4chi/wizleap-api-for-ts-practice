d7156c8470de68b579c3d9c4cb3ceaf94a82f44f
sor4chi <sorakawa0218@gmail.com>
2022-04-25 11:32:33 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: any, res: any) => {
+app.get("/", (req: any, res: any) => {
   try {
     res.send({ name: "hoge" });
   } catch (error) {
@@ -14,8 +13,8 @@ app.get('/', (req: any, res: any) => {
 });
 
 //app.listen(process.env.PORT || 3000);
-process.env.NOW_REGION ? (module.exports = app) : app.listen(PORT);
+process.env.NOW_REGION ? (module.exports = app) : app.listen(3000);
 
-console.log('starts');
+console.log("starts");
 
 export default app;

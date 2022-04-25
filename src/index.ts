41927ba831935588838d9a92c5e53e32347c752b
sor4chi <sorakawa0218@gmail.com>
2022-04-25 11:16:31 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-const express = require("express");
+
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: any, res: any) => {
+app.get('/', (req: any, res: any) => {
   try {
     res.send({ name: "hoge" });
   } catch (error) {
@@ -16,6 +17,6 @@ app.get("/", (req: any, res: any) => {
 app.listen({ port: 3000 }, () => {
   console.log(`Server ready at http://localhost:3000`);
 });
-console.log("starts");
+console.log('starts');
 
 export default app;
